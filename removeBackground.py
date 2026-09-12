71187d29bb41f72f0815de9ca45ac41c17f1f2a4
abdullah O <a330bd@outlook.sa>
2026-09-12 21:16:41 +0300

    Remove Background API Script
    A simple Python script that uses the remove.bg API to automatically remove the background from images.

---
+++ removeBackground.py
@@ -0,0 +1,17 @@
+import requests
+
+response = requests.post(
+    'https://api.remove.bg/v1.0/removebg',
+    # تم وضع اسم الصورة الحقيقي هنا في المدخلات
+    files={'image_file': open('صور-جميلة-رائعة.jpg', 'rb')},
+    data={'size': 'auto'},
+    headers={'X-Api-Key': '8A4rvKMNWvCvkeqhWcj31nGz'},
+)
+
+if response.status_code == requests.codes.ok:
+    # تم تصحيح طريقة كتابة دالة الحفظ وموضع وضع الكتابة الثنائي ('wb')
+    with open('no-bg.png', 'wb') as out:
+        out.write(response.content)
+    print("تمت إزالة الخلفية بنجاح وحفظ الصورة باسم no-bg.png!")
+else:
+    print("Error:", response.status_code, response.text)
